1e9fb1f9098b759094eaea3b7a2093b520b8cb66
Dracevui <liam_aumento@yahoo.com>
2021-04-25 21:37:44 +0100

    Created fruit class and drew it onscreen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import math
+from pygame.math import Vector2
 
 
 def screen_dimensions(width, height):  # Lets me easily change the dimensions of the game window
@@ -17,37 +18,60 @@ def game_quit():  # Quits the game when called upon
     sys.exit()
 
 
-def scale_window():  # Scales the game window and assets to fit the user's monitor dimensions
-    frame = pygame.transform.scale(DUMMY_WINDOW, SCREEN_DIMENSIONS)
-    WINDOW.blit(frame, frame.get_rect())
-    pygame.display.flip()
+class Fruit:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.x = 5
+        self.y = 4
+        self.pos = Vector2(self.x, self.y)
+
+    def draw_fruit(self, cell_size, colour):  # Draws fruit onscreen
+        fruit_rect = pygame.Rect(self.pos.x, self.pos.y, cell_size, cell_size)
+        pygame.draw.rect(self.parent_screen, colour, fruit_rect)
 
 
-def main():  # The main game loop
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_quit()
+class Game:
+    def __init__(self):
+        # Game Initialisation
+        pygame.init()
+        pygame.display.set_caption("Hiss Noises")
 
-        DUMMY_WINDOW.fill((175, 215, 70))
-        scale_window()
-        CLOCK.tick(FPS)
+        # Game Constants
+        self.CELL_SIZE = 40
+        self.CELL_NUMBER = 20
+        self.DUMMY_WINDOW = pygame.Surface((self.CELL_SIZE * self.CELL_NUMBER, self.CELL_SIZE * self.CELL_NUMBER))
+        self.SCREEN_DIMENSIONS = screen_dimensions(self.CELL_SIZE * self.CELL_NUMBER, self.CELL_SIZE * self.CELL_NUMBER)
+        self.WINDOW = pygame.display.set_mode(self.SCREEN_DIMENSIONS)
+        self.CLOCK = pygame.time.Clock()
+        self.FPS = 60
 
+        # Colours
+        self.RED = (255, 0, 0)
 
-# Game Initialisation
-pygame.init()
-pygame.display.set_caption("Hiss Noises")
+        # Game Variables
+        self.running = True
 
-# Constants
-WINDOW_W, WINDOW_H = 400, 500
-DUMMY_WINDOW = pygame.Surface((WINDOW_W, WINDOW_H))
-SCREEN_DIMENSIONS = screen_dimensions(WINDOW_W, WINDOW_H)
-WINDOW = pygame.display.set_mode(SCREEN_DIMENSIONS)
-CLOCK = pygame.time.Clock()
-FPS = 60
+        # Class Imports
+        self.fruit = Fruit(self.DUMMY_WINDOW)
 
-# Game Variables
+    def scale_window(self):  # Scales the game window and assets to fit the user's monitor dimensions
+        frame = pygame.transform.scale(self.DUMMY_WINDOW, self.SCREEN_DIMENSIONS)
+        self.WINDOW.blit(frame, frame.get_rect())
+        pygame.display.flip()
+
+    def main(self):  # The main game loop
+        while self.running:
+            self.DUMMY_WINDOW.fill((175, 215, 70))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game_quit()
+
+            self.fruit.draw_fruit(self.CELL_SIZE, self.RED)
+            self.scale_window()
+            self.CLOCK.tick(self.FPS)
 
 
 if __name__ == '__main__':
-    main()
+    game = Game()
+    game.main()
